c00c536066021cf46098f52991a06ea4ad2810e9
gilberto <gilbertowuwu0@gmail.com>
2025-01-10 17:17:08 -0400

    cambios en la base de datos

--- schema.sql
+++ schema.sql
@@ -8,6 +8,7 @@ CREATE TABLE trabajador(
 	cedula VARCHAR(10) NOT NULL PRIMARY KEY UNIQUE,
 	nombre VARCHAR(20) NOT NULL,
 	apellido VARCHAR(20) NOT NULL,
+	contrasena VARCHAR(30) NOT NULL,
 	rol VARCHAR(30) NOT NULL,
 	horas INT NOT NULL default 0,
 	sueldo INT NOT NULL default 0,
@@ -15,22 +16,12 @@ CREATE TABLE trabajador(
 	estado INT default 0
 );
 
-INSERT INTO trabajador (cedula, nombre, apellido, rol, horas, sueldo, datos_transferencia, estado) VALUES ('890432987','Gustavo', 'Diaz', 'Ingeniero Informatico', 8, 400,  'Mercantil, 0412432809, 74968958',0);
-INSERT INTO trabajador (cedula, nombre, apellido, rol, horas, sueldo, datos_transferencia, estado) VALUES ('342374923','Antonio', 'Jimenez', 'Comerciante', 5, 500, 'Banesco, 0412893482, 38492834',1);
-INSERT INTO trabajador (cedula, nombre, apellido, rol, horas, sueldo, datos_transferencia, estado) VALUES ('384927422','Maria', 'Guzman', 'Expositor', 8, 300,  'Exterior, 0414384832, 9384829', 0);
+INSERT INTO trabajador (cedula, nombre, apellido, contrasena, rol, horas, sueldo, datos_transferencia, estado) VALUES ('890432987','Gustavo', 'Diaz', 'trabajador' ,'Ingeniero Informatico', 8, 400,  'Mercantil, 0412432809, 74968958',0);
+INSERT INTO trabajador (cedula, nombre, apellido, contrasena, rol, horas, sueldo, datos_transferencia, estado) VALUES ('342374923','Antonio', 'Jimenez', 'trabajador' ,'Comerciante', 5, 500, 'Banesco, 0412893482, 38492834',1);
+INSERT INTO trabajador (cedula, nombre, apellido, contrasena, rol, horas, sueldo, datos_transferencia, estado) VALUES ('384927422','Maria', 'Guzman', 'trabajador', 'Expositor', 8, 300,  'Exterior, 0414384832, 9384829', 0);
+INSERT INTO trabajador (cedula, nombre, apellido, contrasena, rol, horas, sueldo, datos_transferencia, estado) VALUES ('83473283','Eric', 'Stephenson', 'Administrador', 'Expositor', 8, 300,  'Venezuela, 0414384832, 9384829', 0);
+INSERT INTO trabajador (cedula, nombre, apellido, contrasena, rol, horas, sueldo, datos_transferencia, estado) VALUES ('34829423','Kyle', 'Mercury', 'Administrador', 'Expositor', 8, 300,  'Mercantil, 0414384832, 9384829', 0);
 
-
--- TABLA PARA LOS ADMINISTRADORES
-
-CREATE TABLE administrador(
-	cedula VARCHAR(10) NOT NULL PRIMARY KEY UNIQUE,
-	nombre VARCHAR(20) NOT NULL,
-	rol VARCHAR(20) NOT NULL,
-	apellido VARCHAR(20) NOT NULL
-);
-
-INSERT INTO administrador (cedula, nombre, apellido, rol) VALUES ("83473283", "Eric", "Stephenson", "administrador");
-INSERT INTO administrador (cedula, nombre, apellido, rol) VALUES ("34829423", "Kyle", "Mercury", "administrador");
 
 
 -- TABLA PARA LOS MENSAJES
@@ -39,7 +30,7 @@ CREATE TABLE mensaje(
 	fk_administrador VARCHAR (10) NOT NULL, 
 	fk_trabajador VARCHAR (10) NOT NULL,
 	contenido VARCHAR (300) NOT NULL,
-	FOREIGN KEY(fk_administrador) REFERENCES administrador(cedula),
+	FOREIGN KEY(fk_administrador) REFERENCES trabajador(cedula),
 	FOREIGN KEY(fk_trabajador) REFERENCES trabajador(cedula),
 	leido BOOLEAN default 0
 );
